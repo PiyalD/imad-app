41138e6d23757c01bc3d5db295fcde5531d6826b
Piyal De <30987338+PiyalD@users.noreply.github.com>
2017-08-17 01:40:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,4 @@ alert('Welcome to Piyal\'s IMAD Profile');
 var element = document.getElementById('main-text');
 element.innerHTML='HI!! This is PIYAL..and this is my first webapp';
 //move the image
-var img=document.getElementById('madi');
-img.onlink = function()
-{
-    img.style.marginLeft= '100px';
-};
+
