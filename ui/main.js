f7229785bf525ba2b10ed77d23ce62e5f22ab78c
Piyal De <30987338+PiyalD@users.noreply.github.com>
2017-08-17 01:21:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,5 @@
 console.log('Loaded!');
 alert('Welcome to Piyal\'s IMAD Profile');
+//chnge the text of main-text div
+var element = document.getElementById('main-text');
+element.innerHTML='HI!! This is PIYAL..aand this is my first webapp';
