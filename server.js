4a767fdc87ec02a89abe52c79017429f719863ab
Piyal De <30987338+PiyalD@users.noreply.github.com>
2017-08-18 03:40:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ return htmlTemplate;
 var counter=0;
 app.get('/counter', function(req, res){
     counter=counter+1;
-    res.send(counter,toString());
+    res.send(counter.toString());
 });
 
 app.get('/', function (req, res) {
