fca05544b95c58833fc1a2b1274506ac78d51a5b
Piyal De <30987338+PiyalD@users.noreply.github.com>
2017-08-18 07:40:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,6 +49,6 @@ submit.onclick = function(){
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
     //make the request
-    request.open('GET', 'http://piyalde2009.imad.hasura-app.io/submit-name?=' + name, true);
+    request.open('GET', 'http://piyalde2009.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
