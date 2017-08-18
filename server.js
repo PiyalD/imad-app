86fb60f1325493a46126ebdb1365dca70f349ffa
Piyal De <30987338+PiyalD@users.noreply.github.com>
2017-08-18 07:11:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,7 +120,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+var names=[];
+app.get('/submit-name?=', function(res,req){ //submit-name?name=xxx
+    //get the name from the request
+   var name= req.query.name;
+   
+   names.push(name);
+   //JSON: JavaScript Object Notation.. used to convert JavaScript objects into strings
+   res.send(JSON.stringify(names));
+});
 
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
@@ -141,15 +149,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names=[];
-app.get('/submit-name?=', function(res,req){ //submit-name?name=xxx
-    //get the name from the request
-   var name= req.query.name;
-   
-   names.push(name);
-   //JSON: JavaScript Object Notation.. used to convert JavaScript objects into strings
-   res.send(JSON.stringify(names));
-});
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
