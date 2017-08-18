f06e1bae96cd504d69782d289f42308db9d4d502
Piyal De <30987338+PiyalD@users.noreply.github.com>
2017-08-18 06:24:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,22 +21,21 @@ button.onclick=function(){
     request.send(null);
 };
 
+//SUBMIT NAME
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
 
-var nameInp = document.getElementById('name');
-var name = nameInp.value;
-
-var submit = document.getElementById('submit_bttn');
-submit.onclick = function()
-{
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
   //send a request to the server with a name as parameter
   
   
   //render the response of list of names into the html code
   var names=['name1', 'name2', 'name3'];
   var list='';
-  for(var i=0; i<names.length; i++){
-     list+= '<li>' + name[i] + '</li><br>';
+  for(var i=0; i< names.length; i++){
+     list+= '<li>' + names[i] + '</li>';
   }
-   var ls=document.getElementById('lst');
-   ls.innerHTML=list;
+   var ul=document.getElementById('namelist');
+   ul.innerHTML=list;
 };
