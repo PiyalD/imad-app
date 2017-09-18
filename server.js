6dfdedce4d52b23768d44c38ee6912e2913e5b4f
Piyal De <30987338+PiyalD@users.noreply.github.com>
2017-09-19 03:53:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config ={
+    user: 'piyalde2009',
+    database: 'piyalde2009',
+    host: 'db.imad.hasura-app.io',
+    port: '5432'
+    password: process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -118,6 +127,20 @@ app.get('/counter', function(req, res){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+var pool = new Pool(config)
+app.get('/test-db', function(req,res){
+    //make a select request
+    //return a response with the results
+    pool.query('SELECT * FROM test', funtion(err,result){
+        if(err){
+            res.status(500).send(err,toString());
+        }else{
+            res.send(JSON.stringify(result));
+        }
+    })
 });
 
 var names=[];
