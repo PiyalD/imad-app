70a26912d7223382094c96bd6ca81fb739a3a39b
Piyal De <30987338+PiyalD@users.noreply.github.com>
2017-09-22 04:22:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ app.get('/submit-name', function(req,res){ //submit-name?name=xxx
 app.get('/article/:articleName', function (req, res) {
     
     
-    pool.query("SELECT * FROM article where title = " + req.params.articleName, function(err, result){
+    pool.query("SELECT * FROM article where title = '" + req.params.articleName + "'", function(err, result){
         if (err){
             res.status(500).send(err.toString());
         } else {
